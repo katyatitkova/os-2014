0994e56cc57df81e569f64248ac87a2a2b6b8ee9
Ekaterina Titkova <katyatitkova@gmail.com>
2014-04-10 23:36:49 +0400

    seems to be working

--- asfdscat/fdscat.c
+++ asfdscat/fdscat.c
@@ -2,26 +2,88 @@
 
 #include <string.h>
 
-const int BUF_SIZE = 4;//1024;
+const int BLOCK_SIZE = 4;//1024;
+const char DELIMETER = '\n';
+
+struct buffer_t
+{
+    char * buf;
+    size_t size; //allocated memory
+    size_t length; //used memory
+};
+
+void buffer_init(struct buffer_t * buf)
+{
+    buf->buf = malloc_(BLOCK_SIZE);
+    buf->size = BLOCK_SIZE;
+    buf->length = 0;
+}
+
+void buffer_realloc(struct buffer_t * buf)
+{
+    buf->size += BLOCK_SIZE;
+    buf->buf = realloc_(buf->buf, buf->size);
+}
+
+void buffer_reverse(struct buffer_t * buf, size_t until)
+{
+    for (size_t i = 0; i <= until / 2; ++i)
+    {
+        char tmp = buf->buf[i];
+        buf->buf[i] = buf->buf[until - i];
+        buf->buf[until - i] = tmp;
+    }
+}
 
 void read_and_write_all(int from, int to)
 {
-    char * buf = malloc_(BUF_SIZE);
-    ssize_t len = 0;
+    struct buffer_t buf;
+    buffer_init(&buf);
     bool eof = false;
     while (!eof)
     {
-        ssize_t r = read_(from, buf + len, BUF_SIZE - len);
+        ssize_t r = read_(from, buf.buf + buf.length, buf.size - buf.length);
         if (r == 0)
             eof = true;
-        len += r;
-        if (len == BUF_SIZE || eof)
+        size_t old_length = buf.length;
+        buf.length += r;
+        size_t i = old_length;
+        while (i < buf.length)
         {
-            write_all(to, buf, len);
-            len = 0;
+            if (buf.buf[i] == DELIMETER)
+            {
+                if (i != 0)
+                    buffer_reverse(&buf, i - 1);
+                /*perror("after reverse");
+                perror(buf.buf);
+                perror("----");*/
+                write_all(to, buf.buf, i + 1);
+                size_t count = buf.length - i - 1;
+                memmove(buf.buf, buf.buf + i + 1, count);
+                /*perror("after memcpy");
+                perror(buf.buf);
+                perror("----");*/
+                memset(buf.buf + count, 0, buf.size - count);
+                /*perror("after memset");
+                perror(buf.buf);
+                perror("----");*/
+                buf.length -= (i + 1);
+                i = 0;
+            }
+            else
+                ++i;
+        }
+        if (buf.length == buf.size && !eof)
+        {
+            buffer_realloc(&buf);
+        }
+        if (eof)
+        {
+            buffer_reverse(&buf, buf.length);
+            write_all(to, buf.buf, buf.length);
         }
     }
-    free(buf);
+    free(buf.buf);
 }
 
 int main(int argc, char * argv[])
@@ -34,3 +96,4 @@ int main(int argc, char * argv[])
     }
     return EXIT_SUCCESS;
 }
+
