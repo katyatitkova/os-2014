9f527dc06d6e2d54b392e84c47bd6d7ad462ba55
Ekaterina Titkova <katyatitkova@gmail.com>
2014-04-16 22:59:07 +0400

    some pseudo code

--- checkmod/checkmod.c
+++ checkmod/checkmod.c
@@ -20,9 +20,12 @@ int main(int argc, char * argv[])
 {
     if (argc < 2 || argc % 2 != 0)
         error_exit("number of arguments");
+    char * uid = argv[1];
+    // check if uid exists?
     for (int i = 2; i < argc; i += 2)
     {
         char * file = argv[i];
+        // check if file exists
         char * mode = argv[i + 1];
         if (!verify_mode(mode))
             error_exit("mode");
