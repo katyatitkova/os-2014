605c90e0e41fe566d2726e036ff3f1d0d5748900
Ekaterina Titkova <katyatitkova@gmail.com>
2014-04-16 22:56:30 +0400

    mode checking

--- checkmod/checkmod.c
+++ checkmod/checkmod.c
@@ -1,11 +1,31 @@
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+
+void error_exit(char * error_msg)
+{
+    perror(error_msg);
+    exit(EXIT_FAILURE);
+}
+
+bool verify_mode(char * mode)
+{
+    return
+        (strlen(mode) == 1 && (mode[0] == 'r' || mode[0] == 'w')) ||
+        (strlen(mode) == 2 && mode[0] == 'r' && mode[1] == 'w');
+}
 
 int main(int argc, char * argv[])
 {
     if (argc < 2 || argc % 2 != 0)
+        error_exit("number of arguments");
+    for (int i = 2; i < argc; i += 2)
     {
-        perror("number of arguments");
-        exit(EXIT_FAILURE);
+        char * file = argv[i];
+        char * mode = argv[i + 1];
+        if (!verify_mode(mode))
+            error_exit("mode");
     }
     return EXIT_SUCCESS;
 }
