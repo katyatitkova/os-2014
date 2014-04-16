e3fd5d185b85354137fe356a9becacdafd700361
Ekaterina Titkova <katyatitkova@gmail.com>
2014-04-17 00:47:05 +0400

    pseudo code fixed

--- checkmod/checkmod.c
+++ checkmod/checkmod.c
@@ -22,7 +22,6 @@ int main(int argc, char * argv[])
     if (argc < 2 || argc % 2 != 0)
         error_exit("number of arguments");
     char * uid = argv[1];
-    // check if uid exists?
     for (int i = 2; i < argc; i += 2)
     {
         char * file = argv[i];
